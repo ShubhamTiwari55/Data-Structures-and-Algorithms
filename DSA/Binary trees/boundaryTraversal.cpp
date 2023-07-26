b82cef8c47f73fda758e8453c79b490aa5e3fd98
ShubhamTiwari55 <stofficial059@gmail.com>
2023-07-26 07:12:16 +0530

    added boundary traversal

--- DSA/Binary trees/boundaryTraversal.cpp
+++ DSA/Binary trees/boundaryTraversal.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<stack>
 #include<climits>
 #include<queue>
 using namespace std;
@@ -62,9 +61,34 @@ void levelOrderTraversal(node* root){
          cout<<endl;
 }
     }
+void leftBoundary(node* root) {
+    if(root==NULL) return;
+    if(root->left==NULL && root->right==NULL) return;
+    cout<<root->val<<" ";
+    leftBoundary(root->left);
+    if(root->left==NULL) leftBoundary(root->right);
+}
+void bottomBoundary(node* root){
+    if(root==NULL) return;
+    if(root->left==NULL && root->right==NULL) cout<<root->val<<" ";
+    bottomBoundary(root->left);
+    bottomBoundary(root->right);
+}
+void rightBoundary(node* root){
+    if(root==NULL) return;
+    if(root->left==NULL && root->right==NULL) return;
+    rightBoundary(root->right);
+    if(root->right==NULL) rightBoundary(root->left);
+    cout<<root->val<<" ";
+}
 int main(){
     int arr[]={1,2,3,4,5,INT_MIN,6,7,INT_MIN,8,INT_MIN,9,10,INT_MIN,11,INT_MIN,12,INT_MIN,13,INT_MIN,14,15,16,INT_MIN,17,INT_MIN,INT_MIN,18,INT_MIN,19,INT_MIN,INT_MIN,INT_MIN,20,21,22,23,INT_MIN,24,25,26,27,INT_MIN,INT_MIN,28,INT_MIN,INT_MIN};
     int n = sizeof(arr)/sizeof(arr[0]);
     node* root = construct(arr,n);
-    levelOrderTraversal(root);
+    //levelOrderTraversal(root);   //for printing the original tree//
+
+    //boundary traversal is done by printing left boundary elements then leaf elements present at bottom then the right boundary elements except the root as it was covered in left boundary elements and printing the right ones in reverse order//
+    leftBoundary(root);
+    bottomBoundary(root);
+    rightBoundary(root->right);
 }
