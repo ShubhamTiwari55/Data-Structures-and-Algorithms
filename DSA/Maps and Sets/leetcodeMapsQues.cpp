c6517a530080862dfc649b0c003f1d07aeb5f261
ShubhamTiwari55 <stofficial059@gmail.com>
2023-07-29 14:55:44 +0530

    maps introduced

---
+++ DSA/Maps and Sets/leetcodeMapsQues.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+//leetcode ques no. 242 -> valid anagram//
+class Solution {
+public:
+//using the concept of frequency array
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        unordered_map<char,int> map1;
+        unordered_map<char,int> map2;
+        for(int i=0;i<s.length();i++){
+            map1[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            map2[t[i]]++;
+        }
+        for(auto x:map1){
+            char ch1 = x.first;
+            int freq1 = x.second;
+            if(map2.find(ch1)!=map2.end()){
+                int freq2 = map2[ch1];
+                if(freq2!=freq1) return false;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
+//method 2..
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        unordered_map<char,int> map1;
+        for(int i=0;i<s.length();i++){
+            map1[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            char ch = t[i];
+            if(map1.find(ch)!=map1.end()){
+                map1[ch]--;
+                if(map1[ch]==0) map1.erase(ch);
+            }
+            else return false;
+        }
+        if(map1.size()>0) return false;
+        return true;
+    }
+};
+int main(){
+}
