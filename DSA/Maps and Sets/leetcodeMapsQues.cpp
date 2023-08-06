3061abd4b173c15d693a11f7a68a910b5c9366e4
ShubhamTiwari55 <stofficial059@gmail.com>
2023-08-06 22:06:09 +0530

    updated maps

--- DSA/Maps and Sets/leetcodeMapsQues.cpp
+++ DSA/Maps and Sets/leetcodeMapsQues.cpp
@@ -89,5 +89,38 @@ public:
         return true;
     }
 };
+
+//leetCode ques no. 2094 -> Finding 3-Digit Even Numbers
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> ans;
+        unordered_map<int,int> m;
+        for(int ele:arr){
+            m[ele]++;
+        }
+        for(int i=100;i<=999;i+=2){
+            int x = i;
+            int a = x%10;  //one's place digit
+            x = x/10;
+            int b = x%10;   //tens place digit
+            x = x/10;
+            int c = x;      //Hundred's place digit
+            if(m.find(a)!=m.end()){
+                    m[a]--;
+                    if(m[a]==0) m.erase(a);
+                    if(m.find(b)!=m.end()){
+                        m[b]--;
+                         if(m[b]==0) m.erase(b);
+                        if(m.find(c)!=m.end()) ans.push_back(i);
+                        m[b]++;
+                    }
+                    m[a]++;
+            }
+        }
+        return ans;
+    }
+};
 int main(){
 }
