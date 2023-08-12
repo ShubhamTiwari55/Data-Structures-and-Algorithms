60865706ccc5bf6c821d5768e3644fba605ad4ba
ShubhamTiwari55 <stofficial059@gmail.com>
2023-08-12 14:28:36 +0530

    ques updated

--- DSA/Binary trees/leetCodeQues1.cpp
+++ DSA/Binary trees/leetCodeQues1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 class TreeNode{
     public:
--- DSA/Maps and Sets/leetcodeMapsQues.cpp
+++ DSA/Maps and Sets/leetcodeMapsQues.cpp
@@ -207,5 +207,28 @@ public:
        return b;
     }
 };
+
+//leetcode ques no. 560 -> subarray sum equals k
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        vector<int> pre(nums.size(),0);
+        pre[0] = nums[0];
+        for(int i=1;i<nums.size();i++){
+            pre[i] = nums[i]+pre[i-1];
+        }
+        unordered_map<int, int> m;
+        int count = 0;
+       for(int i=0;i<nums.size();i++){
+            if(pre[i]==k){
+               count++;
+           }
+               int rem = pre[i]-k;
+               if(m.find(rem)!=m.end()) count+=m[rem];
+                m[pre[i]]++;
+        }
+        return count;
+    }
+};
 int main(){
 }
