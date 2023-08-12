d56b9ab8de73e33d1c6f55fc737209b955ee3544
ShubhamTiwari55 <stofficial059@gmail.com>
2023-08-13 00:41:59 +0530

    maps completed

--- DSA/Maps and Sets/leetcodeMapsQues.cpp
+++ DSA/Maps and Sets/leetcodeMapsQues.cpp
@@ -358,5 +358,28 @@ public:
     }
 };
 
+//leetcode ques no. 1497 -> Check if array pairs are divisible by k//
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        unordered_map<int,int>m;
+        for(int ele:arr){
+            ele = ((ele%k)+k)%k;
+            m[ele]++;
+        }
+        if(m.find(0)!=m.end()){
+            if(m[0]%2!=0) return false;
+            m.erase(0);
+        }
+        for(auto x:m){
+            int ele = x.first;
+            int rem = k-ele;
+            if(m.find(rem)==m.end()) return false;
+            if(m[ele]!=m[rem]) return false;
+        }
+        return true;
+    }
+};
+
 int main(){
 }
