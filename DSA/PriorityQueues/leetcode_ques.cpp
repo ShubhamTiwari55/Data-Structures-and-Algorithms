46eecf1612018689a33b2e881c286e05d3f767f1
ShubhamTiwari55 <stofficial059@gmail.com>
2023-08-14 14:41:07 +0530

    ques added

--- DSA/PriorityQueues/leetcode_ques.cpp
+++ DSA/PriorityQueues/leetcode_ques.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 #include<queue>
 using namespace std;
+class ListNode{
+public:
+    int val;
+    ListNode* next;
+    ListNode(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
 
 //leetcode ques no. 1046 -> last stone weight
 class Solution {
@@ -19,6 +28,35 @@ public:
         return pq.top();
     }
 };
+
+//leetcode ques no. 23 -> merge k sorted lists
+class cmp{
+    public:
+    bool operator()(ListNode* l1, ListNode* l2){
+      return l1->val>l2->val;
+    }
+};
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, cmp >pq;
+        for(int i=0;i<lists.size();i++){
+            if(lists[i]==NULL) continue;
+            pq.push(lists[i]);
+        }
+        ListNode* dummy = new ListNode(-1);
+        ListNode* ptr = dummy;
+        while(!pq.empty()){
+            ListNode* curr = pq.top();
+            pq.pop();
+            if(curr->next!=NULL) pq.push(curr->next);
+            ptr->next = curr;
+            ptr = ptr->next;
+        }
+        return dummy->next;
+    }
+};
+
 
 int main(){
 }
