031eceaf9acf97742d2f13f1a16a720b5c98e4f7
ShubhamTiwari55 <stofficial059@gmail.com>
2023-08-20 16:05:18 +0530

    ques added

--- DSA/greedy/leetcode_Ques.cpp
+++ DSA/greedy/leetcode_Ques.cpp
@@ -49,5 +49,28 @@ public:
     }
 };
 
+//leetcode ques no. 881 -> boats to save people
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int i = 0;
+        int j = people.size()-1;
+        int bt = 0;
+        while(i<=j){
+            if(people[i]+people[j]<=limit){
+                bt++;
+                i++;
+                j--;
+            }
+            else{
+                bt++;
+                j--;
+            }
+        }
+        return bt;
+    }
+};
+
 int main(){
 }
