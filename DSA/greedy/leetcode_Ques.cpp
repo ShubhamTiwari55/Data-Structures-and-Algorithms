eabf9e4e6f21e5f83e47c17ffe21011691deab51
ShubhamTiwari55 <stofficial059@gmail.com>
2023-08-24 15:54:25 +0530

    merge intervals added

--- DSA/greedy/leetcode_Ques.cpp
+++ DSA/greedy/leetcode_Ques.cpp
@@ -177,5 +177,33 @@ public:
     }
 };
 
+//leetcode ques no. 56 -> merge intervals
+// https://leetcode.com/problems/merge-intervals/submissions/
+bool cmp(vector<int> &a, vector<int> &b){
+    return a[1]<b[1];
+}
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> result;
+        //sort on the basis of ending time
+        sort(intervals.begin(), intervals.end(), cmp);
+        int n = intervals.size();
+        result.push_back(intervals[n-1]);
+        for(int i=n-2;i>=0;i--){
+            vector<int> curr = intervals[i];
+            //check curr.end>=last.start --> overlapping
+            if(curr[1]>=result[result.size()-1][0]){
+             result[result.size()-1][0] = min(result[result.size()-1][0], curr[0]);
+             result[result.size()-1][1] = max(result[result.size()-1][1], curr[1]);
+            }
+            else{
+                result.push_back(intervals[i]);
+            }
+        }
+        return result;
+    }
+};
+
 int main(){
 }
