b9efab2b57ea375c6b0d928ee2e0cf20b055b8a7
ShubhamTiwari55 <stofficial059@gmail.com>
2023-08-24 16:18:46 +0530

    ques added

--- DSA/greedy/leetcode_Ques.cpp
+++ DSA/greedy/leetcode_Ques.cpp
@@ -205,5 +205,25 @@ public:
     }
 };
 
+//leetcode ques no. 435 -> Non-Overlapping intervals
+//https://leetcode.com/problems/non-overlapping-intervals/description/
+bool cmp(vector<int> &a, vector<int>&b){
+    return a[1]<b[1];
+}
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int ans = 0;
+        sort(intervals.begin(), intervals.end(), cmp);
+        int lastEle = intervals[0][1];
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]<lastEle) ans++;
+            else lastEle = intervals[i][1];
+        }       
+        return ans;
+    }
+};
+
+
 int main(){
 }
