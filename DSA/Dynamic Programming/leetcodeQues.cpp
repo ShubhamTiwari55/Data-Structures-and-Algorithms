d3a9b2fcbf444b17c9bf2cd76d21289888bd7a1a
ShubhamTiwari55 <stofficial059@gmail.com>
2023-08-27 11:40:18 +0530

    house robber updated

--- DSA/Dynamic Programming/leetcodeQues.cpp
+++ DSA/Dynamic Programming/leetcodeQues.cpp
@@ -28,6 +28,24 @@ public:
     }
 };
 
+//leetcode ques no. 198 -> House Robber
+//https://leetcode.com/problems/house-robber/description/
+class Solution {
+public:
+    vector<int> dp;
+    int f(vector<int> arr, int i){
+        if(i==arr.size()-1) return arr[i];
+        if(i==arr.size()-2) return max(arr[i], arr[i+1]);
+
+        if(dp[i]!=-1) return dp[i];
+        return dp[i] = max((arr[i]+f(arr,i+2)),(0+f(arr,i+1)));
+    }
+    int rob(vector<int>& nums) {
+       dp.clear();
+       dp.resize(105,-1);
+       return f(nums,0);
+    }
+};
 
 int main(){
 }
