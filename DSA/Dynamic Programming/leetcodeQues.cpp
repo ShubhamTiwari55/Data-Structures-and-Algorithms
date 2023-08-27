3dee1d5dd0e919381e62707246a542589af9aa3d
ShubhamTiwari55 <stofficial059@gmail.com>
2023-08-27 12:17:36 +0530

    updated in house robbers

--- DSA/Dynamic Programming/leetcodeQues.cpp
+++ DSA/Dynamic Programming/leetcodeQues.cpp
@@ -33,17 +33,32 @@ public:
 class Solution {
 public:
     vector<int> dp;
-    int f(vector<int> arr, int i){
+    int fTopDown(vector<int> arr, int i){
         if(i==arr.size()-1) return arr[i];
         if(i==arr.size()-2) return max(arr[i], arr[i+1]);
 
         if(dp[i]!=-1) return dp[i];
-        return dp[i] = max((arr[i]+f(arr,i+2)),(0+f(arr,i+1)));
+        return dp[i] = max((arr[i]+fTopDown(arr,i+2)),(0+fTopDown(arr,i+1)));
     }
+
+    int fBottomUp(vector<int> &arr){
+        int n = arr.size();
+        dp.clear();
+        dp.resize(n);
+        //base case
+        dp[n-1] = arr[n-1];
+        dp[n-2] = max(arr[n-2],arr[n-1]);
+        for(int i=n-3;i>=0;i--){
+            dp[i] = max(arr[i]+dp[i+2],0+dp[n-1]);
+        }
+        return dp[0];
+    }
+
     int rob(vector<int>& nums) {
        dp.clear();
        dp.resize(105,-1);
-       return f(nums,0);
+       return fTopDown(nums,0);
+       //return fBottomUP(nums);    //iterative solution
     }
 };
 
