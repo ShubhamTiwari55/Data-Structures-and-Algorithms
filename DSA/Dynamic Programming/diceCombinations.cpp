2544b1d1602e184c7b7c65939b05ca6e555a2134
ShubhamTiwari55 <stofficial059@gmail.com>
2023-08-28 14:44:29 +0530

    updated in ques

--- DSA/Dynamic Programming/diceCombinations.cpp
+++ DSA/Dynamic Programming/diceCombinations.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 #include<vector>
+#define mod 1000000007
 using namespace std;
+vector<int> dp(1000005,-1);
 int f(int x){
     if(x==0) return 1;
+    if(dp[x]!=-1) return dp[x];
     int sum = 0;
     for(int i=1;i<=6;i++){
-        if((x-1)<0) break;
-        sum+=f(x-i);
+        if((x-i)<0) break;
+        sum = (sum%mod + f(x-i)%mod)%mod;
     }
-    return sum;
+    return dp[x] = sum%mod;
 }
 int main(){
     cout<<f(3);
