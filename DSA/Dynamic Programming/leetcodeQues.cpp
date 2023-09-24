89ca2ba40e86aba2e40e1bad96ce3d3ecedf8e3e
ShubhamTiwari55 <stofficial059@gmail.com>
2023-09-24 16:47:35 +0530

    Leetcode ques added

--- DSA/Dynamic Programming/leetcodeQues.cpp
+++ DSA/Dynamic Programming/leetcodeQues.cpp
@@ -130,5 +130,31 @@ public:
     }
 };
 
+//leetcode ques no. 416 -> Partition equal subset sum
+class Solution {
+public:
+    vector<vector<int>> dp;
+    bool f(vector<int>&arr, int i, int k){
+        if(k==0) return true;
+        if(i==arr.size()) return false;
+        if(dp[i][k]!=-1) return dp[i][k];
+        if(arr[i]<=k){
+            //we can either exclude or include
+           return dp[i][k] = f(arr, i+1, k-arr[i]) || f(arr, i+1, k);
+        }
+        else //we have to always exclude
+        return dp[i][k] = f(arr, i+1, k);
+    }
+    bool canPartition(vector<int>& nums) {
+        int s = 0;
+        for(int i=0;i<nums.size();i++){
+            s+=nums[i];
+        }
+        if(s%2!=0) return false;
+        dp.clear();
+        dp.resize(205, vector<int>(20000, -1));
+        return f(nums, 0, s/2);
+    }
+};
 int main(){
 }
