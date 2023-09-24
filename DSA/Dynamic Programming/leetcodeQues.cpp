1359806a42beadee104888f111847876c15db5b3
ShubhamTiwari55 <stofficial059@gmail.com>
2023-09-24 10:28:05 +0530

    added triangle ques

--- DSA/Dynamic Programming/leetcodeQues.cpp
+++ DSA/Dynamic Programming/leetcodeQues.cpp
@@ -86,5 +86,25 @@ public:
         return f(n,k,target);
     }
 };
+
+//leetcode ques. 120 -> Triangles
+class Solution {
+public:
+    vector<vector<int>> grid;
+    vector<vector<int>> dp;
+    int f(int r, int c){
+        if(r == grid.size()-1){
+            return grid[r][c];
+        }
+        if(dp[r][c]!=-1) return dp[r][c];
+        return dp[r][c] = grid[r][c] + min(f(r+1,c),f(r+1,c+1));
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        grid = triangle;
+        dp.clear();
+        dp.resize(203, vector<int>(203,-1));
+        return f(0,0);
+    }
+};
 int main(){
 }
