9193b86323065b2aacfc4df13630b4a7cb5f32ea
ShubhamTiwari55 <stofficial059@gmail.com>
2023-09-29 01:20:06 +0530

    knight probability in chessboard

--- DSA/Dynamic Programming/leetcodeQues.cpp
+++ DSA/Dynamic Programming/leetcodeQues.cpp
@@ -214,5 +214,32 @@ public:
     }
 };
 
+//leetcode ques no. 688 -> Knight probability in chessboard
+class Solution {
+public:
+    double dp[30][30][105]; //row,column,k
+    int m;
+    double f(int i, int j, int k){
+        if(i<0 || j<0 || i>=m || j>=m) return 0;
+        if(k==0) return 1;
+        if(dp[i][j][k]>-0.9) return dp[i][j][k];
+        double ans = 0.0;
+        ans += (0.125)*f(i+2,j+1,k-1);
+        ans += (0.125)*f(i+1,j+2,k-1);
+        ans += (0.125)*f(i-1,j+2,k-1);
+        ans += (0.125)*f(i-2,j+1,k-1);
+        ans += (0.125)*f(i+2,j-1,k-1);
+        ans += (0.125)*f(i+1,j-2,k-1);
+        ans += (0.125)*f(i-2,j-1,k-1);
+        ans += (0.125)*f(i-1,j-2,k-1);
+        return dp[i][j][k] = ans;
+    }
+    double knightProbability(int n, int k, int row, int column) {
+      m=n;
+      memset(dp, -1, sizeof dp);
+      return f(row, column, k);  
+    }
+};
+
 int main(){
 }
