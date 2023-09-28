ed99025582fc30d5234e26e096f6992b62bc8134
ShubhamTiwari55 <stofficial059@gmail.com>
2023-09-28 17:18:43 +0530

    tourist problem and edit distance added

--- DSA/Dynamic Programming/leetcodeQues.cpp
+++ DSA/Dynamic Programming/leetcodeQues.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<climits>
+#include<bits/stdc++.h>
 #include<unordered_map>
 using namespace std;
 
@@ -178,5 +179,40 @@ int n,m;
         return f(0,0);
     }
 };
+
+//leetcode ques no. 72 -> Edit distance 
+class Solution {
+public:
+    int dp[505][505];
+    //Top-down approach
+    int f(string s1, string s2, int i, int j){
+        if(i==s1.size()) return s2.size()-j;
+        if(j==s2.size()) return s1.size()-i;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i]==s2[j]) return dp[i][j]=f(s1, s2, i+1, j+1);
+        return dp[i][j]=min({1+f(s1,s2,i+1,j+1), 1+f(s1,s2,i,j+1), 1+f(s1,s2,i+1,j)});
+    }
+    int minDistance(string s1, string s2) {
+        memset(dp, 0, sizeof dp);
+        //return f(word1, word2, 0, 0);
+        //bottom-up approach
+        for(int i=0;i<s1.size();i++){
+            int j = s2.size();
+            dp[i][j] = s1.size()-i;
+        }
+        for(int j=0;j<s2.size();j++){
+            int i = s1.size();
+            dp[i][j] = s2.size()-j;
+        }
+        for(int i=s1.size()-1;i>=0;i--){
+            for(int j=s2.size()-1;j>=0;j--){
+                if(s1[i]==s2[j]) dp[i][j] = dp[i+1][j+1];
+                else dp[i][j] = min({1+dp[i+1][j+1], 1+dp[i+1][j], 1+dp[i][j+1]});
+            }
+        }
+        return dp[0][0];
+    }
+};
+
 int main(){
 }
