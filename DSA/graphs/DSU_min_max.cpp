19789efde849a06bcbe15adfc3c5435ce270dc53
ShubhamTiwari55 <stofficial059@gmail.com>
2023-10-31 09:24:08 +0530

    DSU_min_max added

--- DSA/graphs/DSU_min_max.cpp
+++ DSA/graphs/DSU_min_max.cpp
@@ -8,16 +8,22 @@ int find(vector<int> &parent, int x){
     return (parent[x]==x)?x:(find(parent,parent[x]));
 }
 
-void Union(vector<int> &parent, vector<int> &rank, int a, int b){
+void Union(vector<int> &parent, vector<int> &sz, vector<int> &minimal, vector<int> &maximal, vector<int> &rank, int a, int b){
     a = find(parent,a);
     b = find(parent,b);
     if(a==b) return;
     if(rank[a]>=rank[b]){
         rank[a]++;
         parent[b] = a;
+        sz[a]+=sz[b];
+        minimal[a] = min(minimal[a], minimal[b]);
+        maximal[a] = max(maximal[a], maximal[b]);
     }else{
         rank[b]++;
         parent[a] = b;
+        sz[b]+=sz[a];
+        minimal[b] = min(minimal[a], minimal[b]);
+        maximal[b] = max(maximal[a], maximal[b]);
     }
 }
 
@@ -27,8 +33,11 @@ int main(){
     //n -> no. of elements, m -> no. of queries
     vector<int> parent(n+1);
     vector<int> rank(n+1,0);
+    vector<int> sz(n+1,1);
+    vector<int> minimal(n+1);
+    vector<int> maximal(n+1);
     for(int i=0;i<=n;i++){
-        parent[i]=i;
+        parent[i]=minimal[i]=maximal[i]=i;
     }
     while(m--){
         string str;
@@ -36,11 +45,12 @@ int main(){
         if(str=="union"){
             int x,y;
             cin>>x>>y;
-            Union(parent, rank, x, y);
+            Union(parent, sz, minimal, maximal, rank, x, y);
         }else{
             int x;
             cin>>x;
-            cout<<find(parent,x);
+            x=find(parent,x);
+            cout<<minimal[x]<<" "<<maximal[x]<<" "<<sz[x]<<endl;
         }
     }
 }
