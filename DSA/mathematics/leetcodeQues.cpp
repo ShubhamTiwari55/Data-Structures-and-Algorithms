8f6292178c33eca41d4c2848c71a0d15165f2a39
ShubhamTiwari55 <stofficial059@gmail.com>
2023-12-06 14:05:00 +0530

    subarray product less than k

--- DSA/mathematics/leetcodeQues.cpp
+++ DSA/mathematics/leetcodeQues.cpp
@@ -127,5 +127,29 @@ public:
     }
 };
 
+// Leetcode ques no. 713 -> Subarray Product Less Than K
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k <= 1) return 0;
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        int product = 1;
+        while (j < n) {
+            product *= nums[j];
+            while (product >= k) {
+                product /= nums[i];
+                i++;
+            }
+            count += (j - i + 1);
+            j++;
+        }
+        return count;
+    }
+};
+
+
 int main(){
 }
