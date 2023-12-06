38e20e31fac81ab77680fc2160c177287d0a87d2
ShubhamTiwari55 <stofficial059@gmail.com>
2023-12-06 22:08:24 +0530

    Distinct Prime Factors of Product of Array added

--- DSA/mathematics/maths&primeLeetcode.cpp
+++ DSA/mathematics/maths&primeLeetcode.cpp
@@ -65,5 +65,48 @@ public:
 	}
 };
 
+// Leetcode ques no. 2521 -> Distinct Prime Factors of Product of Array.
+class Solution {
+public:
+    void fillSieve(vector<bool> &sieve){
+        int n = sieve.size()-1;
+        for(int i=2; i<=sqrt(n); i++){
+            for(int j=i*2; j<=n; j+=i){  // Use 'i' as the increment here
+                sieve[j] = 0;
+            }
+        }
+    }
+
+    int distinctPrimeFactors(vector<int>& nums) {
+        int n = nums.size();
+        int mx = -1;
+        for(int i=0; i<n; i++){
+            mx = max(mx, nums[i]);
+        }
+        vector<bool> sieve(mx + 1, 1);
+        if (mx + 1 > 0) sieve[0] = 0;
+        if (mx + 1 > 1) sieve[1] = 0;
+        fillSieve(sieve);
+        vector<int> primes;
+        for(int i=2; i<=mx; i++){
+            if(sieve[i] == 1) primes.push_back(i);
+        }
+        vector<bool> taken(primes.size(), 0);
+        for(int i=0; i<nums.size(); i++){
+            int ele = nums[i];
+            for(int j=0; j<primes.size(); j++){  // Change 'i' to 'j' here
+                if(primes[j] > ele) break;
+                if(ele % primes[j] == 0) taken[j] = 1;
+            }
+        }
+        int count = 0;
+        for(int i=0; i<taken.size(); i++){
+            if(taken[i] == 1) count++;
+        }
+        return count;
+    }
+};
+
+
 int main(){
 }
