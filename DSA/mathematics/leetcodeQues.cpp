f3e250a4495957bb9a4d6ac6e0481514691e3490
ShubhamTiwari55 <stofficial059@gmail.com>
2023-12-06 12:47:28 +0530

    Max consecutive ones leetcode ques added

--- DSA/mathematics/leetcodeQues.cpp
+++ DSA/mathematics/leetcodeQues.cpp
@@ -66,5 +66,66 @@ public:
     }
 };
 
+// leetcode ques no. 1004 -> max consecutive ones III
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int n = nums.size();
+        int flips = 0, i = 0, j = 0;
+        int maxlen = INT_MIN, len = INT_MIN;
+        while(j<n){
+            if(nums[j]==1) j++;
+            else if(nums[j]==0){
+                if(flips<k){
+                    flips++;
+                    j++;
+                }
+                else{
+                    //calculate len
+                    len = j-i;
+                    maxlen = max(maxlen, len);
+                    while(nums[i]==1) i++;
+                    i++;
+                    j++;
+                }
+            }
+        }
+        len = j-i;
+        maxlen = max(maxlen, len);
+        return maxlen;
+    }
+};
+
+// Leetcode ques no. 1493 -> Longest Subarray of 1's After Deleting One Element
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int k = 1;
+        int n = nums.size();
+        int flips = 0, i = 0, j = 0;
+        int maxlen = INT_MIN, len = INT_MIN;
+        while(j<n){
+            if(nums[j]==1) j++;
+            else if(nums[j]==0){
+                if(flips<k){
+                    flips++;
+                    j++;
+                }
+                else{
+                    //calculate len
+                    len = j-i;
+                    maxlen = max(maxlen, len);
+                    while(nums[i]==1) i++;
+                    i++;
+                    j++;
+                }
+            }
+        }
+        len = j-i;
+        maxlen = max(maxlen, len);
+        return maxlen-1;
+    }
+};
+
 int main(){
 }
